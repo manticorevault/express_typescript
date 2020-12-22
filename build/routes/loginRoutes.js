7d3aa22267e2796eefdee704dd1a9150ac865c7f
manticorevault <artur_seilah@hotmail.com>
2020-12-22 15:00:01 +0000

    Refactored to implement decorators, created a better RouteHandler and started implementing the controllers

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -2,11 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('You are not allowed to visit this page');
+}
 var router = express_1.Router();
 exports.router = router;
-router.get('/login', function (req, res) {
-    res.send("\n    <form method=\"POST\">\n        <div> \n            <label> Email </label>\n            <input name=\"email\"/>\n        </div>\n\n        <div>\n            <label> Password </label>\n            <input name=\"password\"/>\n        </div>\n\n        <button> Submit! </button>\n\n    </form> \n  ");
-});
 router.post('/login', function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
     // Really insecure login logic just to test the routing.
@@ -31,4 +36,7 @@ router.get('/', function (req, res) {
 router.get('/logout', function (req, res) {
     req.session = undefined;
     res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send('Welcome to the protected route!');
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,29 +1,20 @@
-import { Router, Request, Response } from 'express'
+import { Router, Request, Response, NextFunction } from 'express'
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined }
 }
 
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next()
+    return
+  }
+
+  res.status(403)
+  res.send('You are not allowed to visit this page')
+}
+
 const router = Router()
-
-router.get('/login', (req: Request, res: Response) => {
-  res.send(`
-    <form method="POST">
-        <div> 
-            <label> Email </label>
-            <input name="email"/>
-        </div>
-
-        <div>
-            <label> Password </label>
-            <input name="password"/>
-        </div>
-
-        <button> Submit! </button>
-
-    </form> 
-  `)
-})
 
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body
@@ -66,6 +57,10 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined
   res.redirect('/')
+})
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to the protected route!')
 })
 
 export { router }
